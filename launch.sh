dffd6cdd23b9c6a828496f105de096970635ef09
Jamie Wannenburg <jamiewannenburg@gmail.com>
2025-03-25 11:52:44 +0200

    full path

--- launch.sh
+++ launch.sh
@@ -8,19 +8,19 @@
 #
 
 . /usr/local/pisound/scripts/common/start_puredata.sh
-PATCH="pd/padkontrol-looper.pd"
+PATCH="/usr/local/patchbox-modules/imported/padkontrol/padkontrol-looper.pd"
 shift
 
-echo
+echof
 echo "$PATCH"
 echo "$@"
 
-(
+#(
 	# Connect the osc2midi bridge to the MIDI Inputs and to Pure Data.
-	sleep 4
-	/usr/local/pisound-ctl/connect_osc2midi.sh "pisound-ctl"
-	aconnect "pisound-ctl" "Pure Data";
-	aconnect -d "Pure Data:1" "pisound-ctl"
-) &
+#	sleep 4
+	# /usr/local/pisound-ctl/connect_osc2midi.sh "pisound-ctl"
+#	aconnect "pisound-ctl" "Pure Data";
+#	aconnect -d "Pure Data:1" "pisound-ctl"
+#) &
 
 start_puredata "$PATCH" $@
